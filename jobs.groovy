04099fa02444442c3ec1e531f20940c627d5a8e2
olegsel4 <oleg_slesarenko@tut.by>
2017-03-02 18:30:19 +0300

    test1

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,4 @@
-def gitURL="git@github.com:MNT-Lab/mntlab-dsl.git"
+/*def gitURL="git@github.com:MNT-Lab/mntlab-dsl.git"
 def myJob = 'aslesarenka'job("MNTLAB-${student}-main-build-job"){
 parameters{
 
@@ -48,32 +48,27 @@ shell('tar -cvzf ${BRANCH_NAME//\/}_dsl_script.tar.gz script.sh jobs.groovy')	}	
 archiveArtifacts('${BRANCH_NAME//\/}_dsl_script.tar.gz')	
 }	
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-job('Main Build') {
-    publishers {
-        downstreamParameterized {
-            trigger('${CHECK_BOXES}') {
-                condition('UNSTABLE_OR_BETTER')
-                parameters {
-                    currentBuild()
-                }
-            }
-        }
-    }
-}
+}*/
+			def giturl = 'https://github.com/MNT-Lab/mntlab-dsl.git'def 
+			student = 'aslesarenka'job("MNTLAB-${student}-main-build-job") {	
+			parameters {	gitParam('BRANCH_NAME') 
+				    {type('BRANCH')	}	
+				    activeChoiceReactiveParam('TRIGGERED_JOB_NAMES') 
+				    {	choiceType('CHECKBOX')	groovyScript 
+				     {	script('return ["MNTLAB-aslesarenka-child1-build-job", "MNTLAB-aslesarenka-child2-build-job", "MNTLAB-aslesarenka-child3-build-job", "MNTLAB-aslesarenka-child4-build-job"]') 
+				     } }	}	
+				scm {	git(giturl, "\${BRANCH_NAME}")	}	
+				publishers {
+					downstreamParameterized {	
+						trigger('${TRIGGERED_JOB_NAMES}') 
+						{	condition('UNSTABLE_OR_BETTER')	
+						 parameters {	predefinedProp('BRANCH_NAME', '$BRANCH_NAME')	
+							    }	}	}	}}
+for(i in 1..4) {	
+	job("MNTLAB-${student}-child${i}-build-job") 
+	{	scm {	git(giturl, "\${BRANCH_NAME}" )	}	
+	 parameters {	stringParam("BRANCH_NAME", 'origin/aslesarenka') }	
+	 steps {	shell('echo \$BRANCH_NAME')	
+		shell('sh script.sh > output.txt')
+		shell('tar -cvzf ${BRANCH_NAME//[/]}_dsl_script.tar.gz script.sh jobs.groovy')	}	
+	 publishers {archiveArtifacts('${BRANCH_NAME//[/]}_dsl_script.tar.gz') }	}}
