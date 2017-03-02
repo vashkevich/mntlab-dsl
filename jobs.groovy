1a6cf65b017b1e36ef5215e60563f0361c832724
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-03-02 18:10:34 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,48 +1,47 @@
-job("MNTLAB-mburakouski-main-build-job") {
+job("MNTLAB-hvysotski-main-build-job") {
     description 'Building one main job and four child'
     scm {
         github 'MNT-Lab/mntlab-dsl'
     }
     triggers {
-        scm 'H/5 * * * *'
+        scm 'H * * * *'
     }
      parameters {
-        choiceParam('BRANCH_NAME', ['mburakouski (default)', 'master'])
+        choiceParam('BRANCH_NAME', ['hvysotski (default)', 'master'])
     }
     
      parameters {
         activeChoiceReactiveParam('BUILDS_TRIGGER') {
+            filterable()
             choiceType('CHECKBOX')
             groovyScript {
-                script('return ["MNTLAB-mburakouski-child1-build-job", "MNTLAB-mburakouski-child2-build-job", "MNTLAB-mburakouski-child3-build-job", "MNTLAB-mburakouski-child4-build-job"]'
+                script('return ["MNTLAB-hvysotski-child1-build-job", "MNTLAB-hvysotski-child2-build-job", "MNTLAB-hvysotski-child3-build-job", "MNTLAB-hvysotski-child4-build-job"]'
                       )
                 }          
            }   
         }
-
-    for  (i in 1..4){
     
-    job("MNTLAB-mburakouski-child${i}-build-job") {
-    description 'Build and test the app.'
+    for (i in 1..4) {
+    
+    job("MNTLAB-hvysotski-child${i}-build-job") {
+    
     scm {
         github 'MNT-Lab/mntlab-dsl'
-        }
- }
-    
-    triggers {
-        scm('* * * * *')
     }
+    triggers {
+        scm 'H * * * *'
+    }
+     parameters {
+        choiceParam('BRANCH_NAME', ['hvysotski (default)', 'master'])
+    }   
+    
     steps {
         shell('chmod +x script.sh')
         shell('./script.sh')
-		shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
+        shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
         shell('touch output.txt')
-        shell('cat $JENKINS_HOME/jobs/$JOB_NAME/builds/lastSuccessfulBuild/log > output.txt')
+        shell('cat $JENKINS_HOME/jobs/$JOB_NAME/builds/lastSuccessfulBuild/log > output.txt')      
+       }
+     }
     }
-    publishers {
-        archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
-    }
-}
-
-}
-
+  } 
