4c65796cea1e7c5f2b6f3294ec78539226792f9b
Stepan Ivanchic <stepan_ivanchic@epam.com>
2017-03-02 15:39:41 +0300

    1711

--- jobs.groovy
+++ jobs.groovy
@@ -10,25 +10,8 @@ job("MNTLAB-${student}-main-build-job") {
 
 	}
 
-/*	activeChoiceReactiveParam('CHK') {
-                choiceType('CHECKBOX')
-                groovyScript {
-                        script('return ["MNTLAB-${student}-child1-build-job", "MNTLAB-${student}-child2-build-job", "MNTLAB-${student}-child3-build-job", "MNTLAB-${student}-child4-buil$]')
-                }
-        }*/
-
 	scm {
 		git(giturl, "\${BRANCH_NAME}")
-	}
-
-	steps {
-		downstreamParameterized {
-			trigger('$CHK') {
-				parameters {
-					predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-				}
-			}
-		}
 	}
 
 }
