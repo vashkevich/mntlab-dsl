c3798fbc8b149cef461454b626d21a8f08b3f690
Stepan Ivanchic <stepan_ivanchic@epam.com>
2017-03-02 16:47:13 +0300

    11702

--- jobs.groovy
+++ jobs.groovy
@@ -12,7 +12,6 @@ job("MNTLAB-${student}-main-build-job") {
 			filterable()
 			groovyScript {
 				script('return ["MNTLAB-sivanchic-child1-build-job", "MNTLAB-sivanchic-child2-build-job", "MNTLAB-sivanchic-child3-build-job", "MNTLAB-sivanchic-child4-build-job"]')
-				fallbackScript('"fallback choice"')
                 	}
 			referencedParameter('BOOLEAN-PARAM-1')
                         referencedParameter('BOOLEAN-PARAM-2')
