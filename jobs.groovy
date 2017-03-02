412f480a10ed144dfbcdc4435be3a68cc99a19c9
“Andrei <Andrei_Kutsko1@epam.com>
2017-03-02 22:37:02 +0300

    1.15

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,15 @@
 def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
 
 //Groovy script for 
-def command = "git ls-remote -h ${gitURL}"
-def proc = command.execute()
-proc.waitFor()              
-if ( proc.exitValue() != 0 ) {
-println "Error, ${proc.err.text}"
-System.exit(-1)}
-def branches = proc.in.text.readLines().collect { 
-it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')}
-def branche = branches.findAll { item -> item.contains('akutsko') || item.contains('master')}
+//def command = "git ls-remote -h ${gitURL}"
+//def proc = command.execute()
+//proc.waitFor()              
+//if ( proc.exitValue() != 0 ) {
+//println "Error, ${proc.err.text}"
+//System.exit(-1)}
+//def branches = proc.in.text.readLines().collect { 
+//it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')}
+//def branche = branches.findAll { item -> item.contains('akutsko') || item.contains('master')}
 //branche.each { println it }
 
 //Groovy script for main job
@@ -20,7 +20,20 @@ def myJob = freeStyleJob('MNTLAB-akutsko-main-build-job'){
             filterable()
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script(return branche)
+                script("""
+//Groovy script for 
+def command = "git ls-remote -h ${gitURL}"
+def proc = command.execute()
+proc.waitFor()
+if ( proc.exitValue() != 0 ) {
+println "Error, ${proc.err.text}"
+System.exit(-1)}
+def branches = proc.in.text.readLines().collect {
+it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')}
+def branche = branches.findAll { item -> item.contains('akutsko') || item.contains('master')}
+branche.each { println it }
+return branche
+		""")
             }
         }
 	activeChoiceParam('BUILD_TRIGGER') {
