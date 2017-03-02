ce81bf2d7ef081228964833a91470040af15d848
Kaminski <anton_kaminski@epam.com>
2017-03-02 08:46:40 -0500

    ..

--- jobs.groovy
+++ jobs.groovy
@@ -42,7 +42,7 @@ for (number in 1..4){
   job("MNTLAB-${studname}-child${number}-build-job")
       {
       description("Builds child${number}")
-      parameters { stringParam("BRANCH_NAME", 'Hello world!')}
+      parameters { stringParam("BRANCH_NAME")}
       scm {
           git{
 		remote { url("${giturl}")}
