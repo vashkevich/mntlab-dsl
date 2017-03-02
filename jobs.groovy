f4f864ea91f96320f8780f50595abff531fe53d5
kickman2l <geraskopavel@gmail.com>
2017-03-02 14:55:32 +0200

    adding builds triggers

--- jobs.groovy
+++ jobs.groovy
@@ -11,9 +11,9 @@ job('MNTLAB-pheraska-main-build-job')
                 script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
 		}
         }
-	activeChoiceParam('BRANCH_NAME') {
+	activeChoiceParam('BUILDS_TRIGGER') {
             description('Allows to choose branch from repository')
-            choiceType('CHECK_BOXES')
+            choiceType('CHECKBOX')
             groovyScript {
                 script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
 		}
