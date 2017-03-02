b18d1e00cbbdc2c4b3368b596b27c77fe20e2fbf
kickman2l <geraskopavel@gmail.com>
2017-03-02 14:53:47 +0200

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -9,7 +9,14 @@ job('MNTLAB-pheraska-main-build-job')
             choiceType('SINGLE_SELECT')
             groovyScript {
                 script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
-			}
+		}
+        }
+	activeChoiceParam('BRANCH_NAME') {
+            description('Allows to choose branch from repository')
+            choiceType('CHECK_BOXES')
+            groovyScript {
+                script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
+		}
         }
     }
 }
