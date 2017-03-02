f3af7ae0f395b1667f72ff0b8f370a1de229a13d
matvandrew <matv.andrew@gmail.com>
2017-03-02 22:49:59 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,69 +1,73 @@
-student = 'amatveenko'
-job('MNTLAB-' + student + '-main-build-job') {
+job('MNTLAB-amatveenko-main-build-job') {
     scm {
-        github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
+      github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
     }
-    parameters {
-        gitParameterDefinition{
+  parameters {
+    gitParameterDefinition {
             name('BRANCH_NAME')
             type('BRANCH')
             defaultValue('amatveenko')
             selectedValue('DEFAULT')
-            branch('origin/amatveenko')
+            branch('')
             description('')
             branchFilter('')
             tagFilter('')
-            sortMode('NONE')
+            sortMode('ASCENDING')
             useRepository('')
             quickFilterEnabled(false)
-        }
-    activeChoiceReactiveParam('BUILD_TRIGGER') {
+    }
+    activeChoiceReactiveParam('BUILDS_TRIGGER') {
             choiceType('CHECKBOX')
             groovyScript {
                 script('return ["MNTLAB-amatveenko-child1-build-job", "MNTLAB-amatveenko-child2-build-job", "MNTLAB-amatveenko-child3-build-job", "MNTLAB-amatveenko-child4-build-job"]')
             }
-        }
+
     }
-        
-    steps {
-        downstreamParameterized {
-            trigger('$BUILD_TRIGGER') {
-        block {
+  }
+    
+  steps {
+    downstreamParameterized {
+     		trigger('$BUILDS_TRIGGER') {
+                  block {
                     buildStepFailure('FAILURE')
                     failure('FAILURE')
                     unstable('UNSTABLE')
-                }
-                parameters {
-                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-                }
-            }
-        }
-    }     
+                  }
+      			parameters {
+  	                  predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+      			}
+      	}
+ 	  }
+  }
 }
 
+
 for (i = 1; i <5; i++) {
-    job('MNTLAB-' + student + '-child' + i + '-build-job') {
-        scm {
-            github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
-        }
-        parameters {
-            activeChoiceReactiveParam('BRANCH_NAME') {
-                    choiceType('SINGLE_SELECT')
-                    groovyScript {
-                        script('["origin-amatveenko", "origin-master"]')
-                    }
-            }
-        }
-        steps {
-            shell('''#!/bin/bash
-BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
-bash script.sh > output.txt
-tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh output.txt '''
-            )
-        }
-        
-        publishers {
-            archiveArtifacts('*.tar.gz')
-        }
+job('MNTLAB-amatveenko-child' + i + '-build-job') {
+
+    scm {
+        github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
     }
+  parameters {
+        activeChoiceReactiveParam('BRANCH_NAME') {
+        choiceType('SINGLE_SELECT')
+        groovyScript {
+        script('["amatveenko", "master"]')
+        }
+        }
+  }
+
+  steps {
+        shell('''
+          cd $WORKSPACE
+          bash script.sh > output.txt
+          tar -czvf *_dsl_script.tar.gz jobs.groovy
+          ls -lh
+          echo $BRANCH_NAME
+          ''')
+	}
+          publishers {
+          archiveArtifacts('*_dsl_script.tar.gz')
+          }
+}
 }
