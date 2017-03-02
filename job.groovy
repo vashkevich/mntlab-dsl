580c778fc51bf0f40b0c426b0643bedb46ab4e22
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-02 15:46:21 +0300

    job.groovy#1

---
+++ job.groovy
@@ -0,0 +1,44 @@
+def giturl = "https://github.com/MNT-Lab/mntlab-dsl.git"
+def studname = "acherlyonok"
+//create master branch
+
+
+
+
+  for (number in 1..1){
+    job("MNTLAB-${studname}-main${number}-build-job") {
+      description("Builds main${number}")
+      
+      // scm git
+      scm {
+        git {
+          remote { 
+            url("${giturl}")
+          }
+        }
+      
+
+      // build step
+      steps { shell "echo 'hello'"} 
+      }
+    }
+  } 
+
+  for (number in 1..4){
+    job("MNTLAB-${studname}-child${number}-build-job") {
+      description("Builds child${number}")
+      
+      // scm git
+      scm {
+        git {
+          remote { 
+            url("${giturl}")
+          }
+        }
+      
+
+      // build step
+      steps { shell "echo 'hello'"} 
+      }
+    }
+  } 
