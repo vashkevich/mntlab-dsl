6230030ea2967a27dcc8da6cf68480e195add947
kickman2l <geraskopavel@gmail.com>
2017-03-02 17:10:22 +0200

    qweqweqwe

--- jobs.groovy
+++ jobs.groovy
@@ -37,7 +37,8 @@ for (i = 1; i < 5; i++)
         {
            shell('chmod +x script.sh')
            shell('./script.sh > output.txt')
-           shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
+           //shell('tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
+	   shell('tar -czf test_dsl_script.tar.gz jobs.groovy script.sh')
         }
 
         publishers
