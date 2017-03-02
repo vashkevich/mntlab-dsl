95b467f773f395bc10e55a329460595c8f1775d4
kickman2l <geraskopavel@gmail.com>
2017-03-02 14:29:57 +0200

    fsdasdfs

--- jobs.groovy
+++ jobs.groovy
@@ -8,7 +8,7 @@ job('MNTLAB-pheraska-main-build-job')
             description('Allows to choose branch from repository')
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('["choice1", "choice2"]')
+                script('println ["choice1", "choice2"]')
                        //def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it)};return brnchList.unique();')
                        }
         }
