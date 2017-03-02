fbe5f21cac5e5c03d5677228de2cca42f0e2bc9b
vashkevich <7855862@gmail.com>
2017-03-02 23:35:54 +0300

    it should work

---
+++ jobs.groovy
@@ -0,0 +1,87 @@
+job("MNTLAB-rvashkevich-main-build-job") {
+scm {
+github('MNT-Lab/mntlab-dsl','rvashkevich')
+}
+parameters {
+/* activeChoiceParam('BRANCH_NAME') {
+description('Allows user choose from multiple choices')
+choiceType('SINGLE_SELECT')
+groovyScript {
+script('return["rvashkevich", "master"];')
+}
+}
+*/
+gitParameterDefinition {
+name('BRANCH_NAME')
+type('BRANCH')
+branch('rvashkevich')
+defaultValue('rvashkevich')
+selectedValue('DEFAULT')
+
+description('')
+branchFilter('')
+tagFilter('')
+sortMode('NONE')
+useRepository('')
+quickFilterEnabled(false)
+}
+
+
+
+
+
+activeChoiceReactiveParam('BUILDS_TRIGGER') {
+choiceType('CHECKBOX')
+groovyScript {
+script('return ["MNTLAB-rvashkevich-child1-build-job", "MNTLAB-rvashkevich-child2-build-job", "MNTLAB-rvashkevich-child3-build-job", "MNTLAB-rvashkevich-child4-build-job"]')
+} 
+} 
+}
+steps {
+downstreamParameterized {
+trigger('$BUILDS_TRIGGER') {
+block {
+buildStepFailure('FAILURE')
+failure('FAILURE')
+unstable('UNSTABLE')
+}
+
+parameters {
+predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+}
+}
+}
+}
+
+for (i in 1..4) {
+
+job("MNTLAB-rvashkevich-child${i}-build-job") {
+
+scm {
+github('MNT-Lab/mntlab-dsl','rvashkevich')
+}
+
+parameters {
+activeChoiceParam('BRANCH_NAME') {
+description('Allows user choose from multiple choices')
+choiceType('SINGLE_SELECT')
+groovyScript {
+script('return["origin/rvashkevich", "master"]')
+}
+}
+} 
+
+steps {
+shell('''
+BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
+tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh
+bash script.sh > output.txt ''')
+}
+
+/*shell('chmod +x script.sh')
+shell('./script.sh') 
+shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
+shell('bash script.sh > output.txt')*/ 
+} 
+}
+}
