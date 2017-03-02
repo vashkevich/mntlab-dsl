462ce39ea26555565e19e61868b0785a85e3e157
Anton Kaminski <anton_kaminski@epam.com>
2017-03-02 05:13:15 -0500

    mod jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,12 +1,24 @@
 def giturl = "https://github.com/MNT-Lab/mntlab-dsl.git"
 def studname = "akaminski"
+//create master branch
+job("MNTLAB-${studname}-main-build")
+{
+		
+}
+
+
+
+
+
+
 for (number in 1..4){
   job("MNTLAB-${studname}-child${number}-build-job")
       {
       description("Builds child${number}")
     	scm {
           github("${giturl}")
-    		}
+    	    }
+	steps { shell"sh script.sh" }
     	
 	}
 } 
