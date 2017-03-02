cd57c53010d80586000742c694bd9669e693eaeb
Kaminski <anton_kaminski@epam.com>
2017-03-02 10:30:10 -0500

    ..

--- jobs.groovy
+++ jobs.groovy
@@ -1,10 +1,38 @@
+class Branch_Script {
+			def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+			def command = "git ls-remote -h ${gitURL}"
+                        def proc = command.execute()
+                        proc.waitFor()              
+                        if ( proc.exitValue() != 0 ) {
+                                 println "Error, ${proc.err.text}"
+                                 System.exit(-1)}
+                                 def branches = proc.in.text.readLines().collect { 
+                                 it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')}
+                                 def name = branches.findAll { item -> item.contains('akutsko') || item.contains('master')}
+                                 name.each { println it }
+}
+
+
+
+
+
 def giturl = "https://github.com/MNT-Lab/mntlab-dsl.git"
 def studname = "akaminski"
 
 
 //create master branch
 job("MNTLAB-${studname}-main-build")
-{       parameters {
+{
+       parameters {
+		activeChoiceParam('BRANCH_NAME') {
+            description('You can choose name of branch from GitHub repository')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('import Branch_Script')
+                fallbackScript('"fallback choice"')
+            }
+        }
 		gitParam('BRANCH_NAME'){
 			 type('BRANCH')
 			 defaultValue('akaminski')
