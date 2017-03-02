685c2651e6752f5a3eee82b8e3654bc91bcd7edc
hopetds <hopetds@gmail.com>
2017-03-02 20:12:04 +0300

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf $WORKSPACE/*
-BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 3-)
+BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
 wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/master/script.sh
 chmod +x script.sh
 ./script.sh>>output.txt
