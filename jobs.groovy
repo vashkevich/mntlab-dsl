6c00cbd6f492298fa6a535a0d3cc8a16c158ac3a
kickman2l <geraskopavel@gmail.com>
2017-03-02 16:24:24 +0200

    test

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,3 @@
-def studentName = "pheraska"
 job('MNTLAB-pheraska-main-build-job') 
 {
     scm {
@@ -9,15 +8,15 @@ job('MNTLAB-pheraska-main-build-job')
             description('Allows to choose branch from repository')
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('return [' + studentName +',"master"]')
-			}
+                script('return ["pheraska","master"]')
+	    }
         }
 	activeChoiceParam('BUILDS_TRIGGER') {
             description('Allows to choose branch from repository')
             choiceType('CHECKBOX')
             groovyScript {
                 script('def jobsNamesArray = [];for (i = 1; i < 5; i++){ jobsNamesArray.push("MNTLAB-pheraska-child${i}-build-job");};return jobsNamesArray;')
-			}
+	    }
         }
     }        
 }
