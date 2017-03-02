ec6f2189fe0618ff54bc26c21f8b052f1540b2a3
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-03-02 17:50:16 +0300

    Create jobs.groovy

---
+++ jobs.groovy
@@ -0,0 +1,47 @@
+job("MNTLAB-hvysotski-main-build-job") {
+    description 'Building one main job and four child'
+    scm {
+        github 'MNT-Lab/mntlab-dsl'
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+     parameters {
+        choiceParam('BRANCH_NAME', ['hvysotski (default)', 'master'])
+    }
+    
+     parameters {
+        activeChoiceReactiveParam('BUILDS_TRIGGER') {
+            filterable()
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('return ["MNTLAB-hvysotski-child1-build-job", "MNTLAB-hvysotski-child2-build-job", "MNTLAB-hvysotski-child3-build-job", "MNTLAB-hvysotski-child4-build-job"]'
+                      )
+                }          
+           }   
+        }
+    
+    for (i in 1..4) {
+    
+    job("MNTLAB-hvysotski-child${i}-build-job") {
+    
+    scm {
+        github 'MNT-Lab/mntlab-dsl'
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+     parameters {
+        choiceParam('BRANCH_NAME', ['hvysotski (default)', 'master'])
+    }   
+    
+    steps {
+        shell('chmod +x script.sh')
+        shell('./script.sh')
+        shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
+        shell('touch output.txt')
+        shell('cat $JENKINS_HOME/jobs/$JOB_NAME/builds/lastSuccessfulBuild/log > output.txt')      
+       }
+     }
+    }
+  }     
