bcf59b8d1d554ad4e09a33d2ed8ace9d369722d7
ivanOKman <ivanOKman@tut.by>
2017-03-02 22:56:41 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -72,10 +72,16 @@ job("MNTLAB-imanzhulin-main-build-job") {
     }   
     
     steps {
-         shell('chmod +x script.sh')
+        shell('''
+BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
+tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh
+bash script.sh > output.txt ''')
+        }
+  
+	/*shell('chmod +x script.sh')
         shell('./script.sh')    
         shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
-        shell('bash script.sh > output.txt')  
+        shell('bash script.sh > output.txt')*/  
        }        
      }
     }
