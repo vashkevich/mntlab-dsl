40441ad279dd8781f1af0840269044382b1668d0
mordaBy <bilun.art@gmail.com>
2017-03-02 18:51:08 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -47,7 +47,7 @@ for (i = 1; i <5; i++) {
         }
         steps {
             shell('''#!/bin/bash 
-BRANCH_NAME = $(echo $BRANCH_NAME | cut -c 8-)
+$BRANCH_NAME = $(echo $BRANCH_NAME | cut -c 8-)
 tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy
 bash script.sh > output.txt'''
           	)
