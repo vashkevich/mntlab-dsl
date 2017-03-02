12349b6fb273d60d99cc1a0bab39b1948335b7f6
burakovsky <maksim_burakouski@epam.com>
2017-03-02 22:35:00 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,44 +1,46 @@
 job("MNTLAB-mburakouski-main-build-job") {
     scm {
         github ('MNT-Lab/mntlab-dsl', '*/${BRANCH_NAME}')
-            }
-    triggers {
-       scm 'H * * * *'
-            }
-    parameters {
+    }
+    
+   // triggers {
+   //     scm 'H * * * *'
+   // }
+    
+     parameters {
         choiceParam('BRANCH_NAME', ['mburakouski', 'master'])
-        activeChoiceReactiveParam('BUILDS_TRIGGER') {
+                }
+        parameters {
+                activeChoiceReactiveParam('BUILDS_TRIGGER') {
                 choiceType('CHECKBOX')
                 groovyScript {
                 script('return ["MNTLAB-mburakouski-child1-build-job", "MNTLAB-mburakouski-child2-build-job", "MNTLAB-mburakouski-child3-build-job", "MNTLAB-mburakouski-child4-build-job"]')
-                             }
                 }
-            }
-
-   steps {
-     downstreamParameterized {
-       trigger('$BUILDS_TRIGGER'){
-            parameters {
+             }
+           }
+        steps {
+        downstreamParameterized {
+            trigger('$BUILDS_TRIGGER'){
+                parameters {
                     predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
                         }
+                    }
                 }
-        }
-    }
-}
-
-for (i in 1..4) {
-
-job("MNTLAB-mburakouski-child${i}-build-job") {
-
- scm {
-   github 'MNT-Lab/mntlab-dsl','${BRANCH_NAME}'
-     }
-triggers {
-     scm 'H * * * *'
+            }
          }
 
 
-parameters {
+for (i in 1..4) {
+
+ job("MNTLAB-mburakouski-child${i}-build-job") {
+
+ scm {
+     github 'MNT-Lab/mntlab-dsl','${BRANCH_NAME}'
+ }
+// triggers {
+  //   scm 'H * * * *'
+// }
+  parameters {
         gitParameterDefinition{
             name('BRANCH_NAME')
             type('BRANCH')
@@ -51,17 +53,17 @@ parameters {
             sortMode('NONE')
             useRepository('')
             quickFilterEnabled(false)
-                }
-         }
+        }
+    }
 
  steps {
      shell('touch output.txt')
      shell('chmod +x script.sh')
-     shell('./script.sh > output.txt')
+     shell('./script.sh >> output.txt')
      shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
-          }
+    }
       publishers {
      archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz, output.txt')
-         }
      }
+   }
  }
