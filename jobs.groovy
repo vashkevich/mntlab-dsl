6566bbfc228865a771186288ef6f00b2a924e49d
root <root@epbyminw2033t1.minsk.epam.com>
2017-03-02 17:43:13 +0300

    dfff

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,11 @@
-job("xxxxx", Closure closure=null)
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('https://github.com/MNT-Lab/mntlab-dsl')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+   // steps {
+    //    maven('-e clean test')
+    //}
+}
