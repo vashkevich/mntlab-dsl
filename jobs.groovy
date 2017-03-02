88b238e838433db698e25c05bda94313b7d3bb35
root <root@epbyminw2033t1.minsk.epam.com>
2017-03-02 21:24:44 +0300

    Thu Mar  2 21:24:44 +03 2017

--- jobs.groovy
+++ jobs.groovy
@@ -1,12 +1,19 @@
+  job('MNTLAB-yskrabkou-main-build-job') 
+    {
+        parameters
+        {
+            activeChoiceParam('BRANCH_NAME')
+	        {
+                description('Allows to choose branch from repository')
+                choiceType('SINGLE_SELECT')
+                groovyScript
+                {
+                    script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
+                }
+            }
+        }  
 
-  job('MNTLAB-yskrabkou-main-build-job') {
-        scm {
-            git("git://github.com/MNT-Lab/mntlab-dsl.git")
+      scm
+        {
+            github('MNT-Lab/mntlab-dsl', '${BRANCH_NAME}')
         }
-         triggers {
-        scm('H/15 * * * *')
-    }
-        steps {
-            //maven("test -Dproject.name=${project}/${branchName}")
-        }
-    }
