2ee4c373e7884fc7da6668b35ce39c5c18a1992a
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-02 18:06:48 +0300

    added gitParam

--- jobs.groovy
+++ jobs.groovy
@@ -6,27 +6,27 @@ def studname = "acherlyonok"
     job("MNTLAB-${studname}-main-build-job") {
       description("Builds main${number}")
       
+      // This project is parameterized: activeChoiceReactiveParam
+      parameters {
+          activeChoiceReactiveParam('BUILDS_TRIGGER') {
+              description('Allows user choose from multiple choices')
+              choiceType('CHECKBOX')
+              groovyScript {
+                  script('def used_jobs = ["MNTLAB-ac-child1-build-job", "MNTLAB-ac-child2-build-job", "MNTLAB-ac-child3-build-job", "MNTLAB-ac-child4-build-job", "MNTLAB-ac-child5-build-job"] \n return used_jobs')
+              }
+          }
 
-    parameters {
-        activeChoiceReactiveParam('BUILDS_TRIGGER') {
-            description('Allows user choose from multiple choices')
-            filterable()
-            choiceType('CHECKBOX')
-            groovyScript {
-                script('def used_jobs = ["MNTLAB-ac-child1-build-job", "MNTLAB-ac-child2-build-job", "MNTLAB-ac-child3-build-job", "MNTLAB-ac-child4-build-job", "MNTLAB-ac-child5-build-job"] \n return used_jobs')
-              //  fallbackScript('"fallback choice"')
-            }
-          //  referencedParameter('BOOLEAN-PARAM-1')
-          //  referencedParameter('BOOLEAN-PARAM-2')
-        }
-    }
-
+          gitParam {
+            branch('origin/acherlyonok')
+          }
+      }
 
 
       // scm git
       scm {
         git {
           remote { 
+
             url("${giturl}")
           }
         }
@@ -38,8 +38,10 @@ def studname = "acherlyonok"
       triggers {
         scm 'H/5 * * * *'
       }
+
     }
   }
+
 
   for (number in 1..4){
     job("MNTLAB-${studname}-child${number}-build-job") {
@@ -56,7 +58,7 @@ def studname = "acherlyonok"
 
       // build step
         steps { 
-          shell "echo 'hello'"
+          shell(readFileFromWorkspace('script.sh'))
         }
       }
     }
