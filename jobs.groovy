41d1fd1e93f3ed12393451a1c4e9e00bfe27181e
kickman2l <geraskopavel@gmail.com>
2017-03-02 15:48:54 +0200

    check before create child

--- jobs.groovy
+++ jobs.groovy
@@ -9,23 +9,14 @@ job('MNTLAB-pheraska-main-build-job')
             choiceType('SINGLE_SELECT')
             groovyScript {
                 script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def brnchList = ["pheraska"];def hd = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique();hd.each{ brnchList.push(it);};return brnchList.unique();')
-		}
+	    }
         }
 	activeChoiceParam('BUILDS_TRIGGER') {
             description('Allows to choose branch from repository')
             choiceType('CHECKBOX')
             groovyScript {
                 script('def jobsNamesArray = [];for (i = 0; i <4; i++){ jobsNamesArray.push("MNTLAB-pheraska-child${i}-build-job");};return jobsNamesArray;')
-		}
+	    }
         }
-    }
-    steps {
-    downstreamParameterized {
-        trigger('$BUILDS_TRIGGER') {
-            parameters {
-                predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-            }
-        }
-    }
     }
 }
