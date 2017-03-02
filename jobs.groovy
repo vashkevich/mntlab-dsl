c009d0c5a7d4af93a01eeed2f717039ef876a83a
Stepan Ivanchic <stepan_ivanchic@epam.com>
2017-03-02 12:57:53 +0300

    1937

--- jobs.groovy
+++ jobs.groovy
@@ -1,8 +1,14 @@
 def giturl = 'https://github.com/MNT-Lab/mntlab-dsl.git'
 def student = 'sivanchic'
 
+job("MNTLAB-${student}-main-build-job") {
+        scm {
+            git(giturl)
+        }
+    }
+
 for(i in 1..4) {
-    job("MNTLAB-{student}-child${i}-build-job") {
+    job("MNTLAB-${student}-child${i}-build-job") {
         scm {
             git(giturl)
         }
