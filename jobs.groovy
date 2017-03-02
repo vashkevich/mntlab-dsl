c7b7356c5c878debf4e62d937873a5c9487a7cd4
kickman2l <geraskopavel@gmail.com>
2017-03-02 13:06:38 +0200

    test creating select

--- jobs.groovy
+++ jobs.groovy
@@ -1,11 +1,26 @@
-def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
-def branchesList = ["pheraska"]
-
-def hd = getTags.text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')  }.unique()
-
-hd.each
+job('MNTLAB-pheraska-main-build-job') 
 {
-    branchesList << it
+    scm {
+        github('MNT-Lab/mntlab-dsl', 'pheraska')
+    }
+    parameters {
+        activeChoiceParam('BRANCH_NAME') {
+            description('Allows to choose branch from repository')
+            filterable()
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('
+                       def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute()
+                       def branchesList = ["pheraska"]
+                       def hd = getTags.text.readLines().collect {it.split()[1].replaceAll('refs/heads/', '')  }.unique()
+                       hd.each
+                       {
+                           branchesList << it
+                       }
+                       return branchesList.unique()
+                     ')
+            }
+        }
+    }
 }
 
-return branchesList.unique()
