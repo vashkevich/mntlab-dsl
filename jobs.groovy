7c421c59b2112711ce69730b071345e1874e9e89
Kaminski <anton_kaminski@epam.com>
2017-03-02 10:17:39 -0500

    ..

--- jobs.groovy
+++ jobs.groovy
@@ -9,7 +9,7 @@ job("MNTLAB-${studname}-main-build")
 			 type('BRANCH')
 			 defaultValue('akaminski')
 					 }
-		activeChoiceReactiveParam('CHOICE'){
+		activeChoiceParam('CHOICE'){
 			choiceType('CHECKBOX')
 			groovyScript {
 				script('["MNTLAB-akaminski-child1-build-job", "MNTLAB-akaminski-child2-build-job", "MNTLAB-akaminski-child3-build-job", "MNTLAB-akaminski-child4-build-job"]')
