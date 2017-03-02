ed466c55ac24ff22f37220ef371ba030d4dc4fee
root <root@epbyminw2033t1.minsk.epam.com>
2017-03-02 22:56:10 +0300

    Thu Mar  2 22:56:10 +03 2017

--- jobs.groovy
+++ jobs.groovy
@@ -2,16 +2,18 @@ mybranch = 'yskrabkou'
 
 job('MNTLAB-' + mybranch + '-main-build-job') 
 	{
-		 logRotator {
-        numToKeep(5)
-        artifactNumToKeep(1)
-    }
+		logRotator 
+		{
+        	numToKeep(10)        
+        }
+
 		parameters
         {        	 
             activeChoiceParam('BRANCH_NAME')
 	        {
                 description('Allows to choose branch from repository')
                 choiceType('SINGLE_SELECT')
+
                 groovyScript
                 {
                     script('def getTags = ("git ls-remote -t -h https://github.com/MNT-Lab/mntlab-dsl.git").execute();def branchList  = getTags.text.readLines().collect {it.split()[1].replaceAll("refs/heads/", "")}.unique(); branchList  = branchList .reverse(); return branchList;')
@@ -22,6 +24,7 @@ job('MNTLAB-' + mybranch + '-main-build-job')
          activeChoiceReactiveParam('JOB_SELECT')
           {
          	choiceType('CHECKBOX')
+
             groovyScript 
             {
             script('return ["MNTLAB-yskrabkou-child1-build-job", "MNTLAB-yskrabkou-child2-build-job", "MNTLAB-yskrabkou-child3-build-job", "MNTLAB-yskrabkou-child4-build-job"]')
@@ -55,3 +58,19 @@ job('MNTLAB-' + mybranch + '-main-build-job')
                 }
             }
     }
+
+
+for (i = 1..4) 
+{
+	job('MNTLAB-' + mybranch + '-child' + i + '-build-job') 
+	{
+		parameters {
+            activeChoiceReactiveParam('BRANCH_NAME') {
+                    choiceType('SINGLE_SELECT')
+                    groovyScript {
+                        script('["origin-abilun", "origin-master"]')
+                    }
+            }
+        }
+	}
+}
