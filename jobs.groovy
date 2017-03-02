e0f7a05b8f1c7b9701fe03d2ab677814b7000e1f
burakovsky <maksim_burakouski@epam.com>
2017-03-02 22:29:53 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,46 +1,41 @@
-        job("MNTLAB-mburakouski-main-build-job") {
+job("MNTLAB-mburakouski-main-build-job") {
     scm {
         github ('MNT-Lab/mntlab-dsl', '*/${BRANCH_NAME}')
             }
-   // triggers {
-   //     scm 'H * * * *'
-   // }
-     parameters {
+    triggers {
+       scm 'H * * * *'
+            }
+    parameters {
         choiceParam('BRANCH_NAME', ['mburakouski', 'master'])
         activeChoiceReactiveParam('BUILDS_TRIGGER') {
                 choiceType('CHECKBOX')
                 groovyScript {
                 script('return ["MNTLAB-mburakouski-child1-build-job", "MNTLAB-mburakouski-child2-build-job", "MNTLAB-mburakouski-child3-build-job", "MNTLAB-mburakouski-child4-build-job"]')
+                             }
                 }
-             }
-}
+            }
 
-        steps {
-        downstreamParameterized {
-            trigger('$BUILDS_TRIGGER'){
-                parameters {
+   steps {
+     downstreamParameterized {
+       trigger('$BUILDS_TRIGGER'){
+            parameters {
                     predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
                         }
                 }
         }
-
-
     }
-
-
 }
 
+for (i in 1..4) {
 
-        for (i in 1..4) {
-
- job("MNTLAB-mburakouski-child${i}-build-job") {
+job("MNTLAB-mburakouski-child${i}-build-job") {
 
  scm {
-     github 'MNT-Lab/mntlab-dsl','*/${BRANCH_NAME}'
- }
-// triggers {
-  //   scm 'H * * * *'
-// }
+   github 'MNT-Lab/mntlab-dsl','*/${BRANCH_NAME}'
+     }
+triggers {
+     scm 'H * * * *'
+         }
 
 
 parameters {
@@ -56,17 +51,17 @@ parameters {
             sortMode('NONE')
             useRepository('')
             quickFilterEnabled(false)
-        }
-    }
+                }
+         }
 
  steps {
      shell('touch output.txt')
      shell('chmod +x script.sh')
      shell('./script.sh > output.txt')
      shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
-    }
+          }
       publishers {
      archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz, output.txt')
+         }
      }
-  }
  }
