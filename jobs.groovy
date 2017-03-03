01f3b33255e64cdf8d0cee377d35695afde0f0a7
Heorhi Vysotski <heorhi_vysotski@epam.com>
2017-03-03 12:32:26 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,7 +1,8 @@
-job("MNTLAB-hvysotski-main-build-job") {
+job("MNTLAB-hvysotski-main-build-job") 
+{
     scm {
         github ('MNT-Lab/mntlab-dsl', '*/${BRANCH_NAME}')
-    }
+        }
 	
    // triggers {
    //     scm 'H * * * *'
@@ -11,59 +12,69 @@ job("MNTLAB-hvysotski-main-build-job") {
         choiceParam('BRANCH_NAME', ['hvysotski', 'master'])
                 }
         parameters {
-                activeChoiceReactiveParam('BUILDS_TRIGGER') {
+                activeChoiceReactiveParam('BUILDS_TRIGGER')
+		{
                 choiceType('CHECKBOX')
                 groovyScript {
                 script('return ["MNTLAB-hvysotski-child1-build-job", "MNTLAB-hvysotski-child2-build-job", "MNTLAB-hvysotski-child3-build-job", "MNTLAB-hvysotski-child4-build-job"]')
+                             }
                 }
-             }
-           }
-        steps {
-        downstreamParameterized {
-            trigger('$BUILDS_TRIGGER'){
-                parameters {
-                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-                        }
                     }
+      steps 
+	{
+        downstreamParameterized 
+           {
+            trigger('$BUILDS_TRIGGER')
+	      {
+                block {
+                        //main job fails if any step in child jobs fails
+                        buildStepFailure('FAILURE')
+                        //main job fails if any child job fails
+                        failure('FAILURE')
+                        unstable('UNSTABLE')
+                      }
+                parameters 
+		      {
+                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+		      }
+              }                
+           }
+        }
+   }
+
+for (i in 1..4)
+{
+    job('MNTLAB-hvysotski-child' + i + '-build-job')
+    {
+        parameters
+        {
+            activeChoiceParam('BRANCH_NAME')
+                {
+                description('Allows to choose branch from repository')
+                choiceType('SINGLE_SELECT')
+                groovyScript
+                {
+                script(readFileFromWorkspace('second.groovy'))
                 }
             }
-         }
+        }
 
+        scm
+        {
+            github('MNT-Lab/mntlab-dsl', '${BRANCH_NAME}')
+        }
 
-for (i in 1..4) {
+        steps
+        {
+           shell('''chmod +x script.sh
+                 ./script.sh > output.txt
+                 tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh''')
+        }
 
- job("MNTLAB-hvysotski-child${i}-build-job") {
-
- scm {
-     github 'MNT-Lab/mntlab-dsl','${BRANCH_NAME}'
- }
-// triggers {
-  //   scm 'H * * * *'
-// }
-  parameters {
-        gitParameterDefinition{
-            name('BRANCH_NAME')
-            type('BRANCH')
-            defaultValue('hvysotski')
-            selectedValue('DEFAULT')
-            branch('origin/hvysotski')
-            description('')
-            branchFilter('')
-            tagFilter('')
-            sortMode('NONE')
-            useRepository('')
-            quickFilterEnabled(false)
+        publishers
+        {
+            archiveArtifacts('output.txt')
+            archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
         }
     }
-
- steps {
-     shell('touch output.txt')
-     shell('chmod +x script.sh')
-     shell('./script.sh >> output.txt')
-     shell('tar cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh')
-    }
-      publishers {
-     archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz, output.txt')
-     }
-   }
- }
+}
