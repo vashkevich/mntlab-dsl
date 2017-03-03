80e9426f0e0eb405d7dc9cb75c49c7ec301c7312
burakovsky <maksim_burakouski@epam.com>
2017-03-03 11:58:42 +0300

    Create work.groovy

---
+++ work.groovy
@@ -0,0 +1,26 @@
+import groovy.io.FileType
+
+def listhuist = []
+listhuist.add("mburakouski")
+
+def gitURL = "https://github.com/MNT-Lab/mntlab-dsl"
+def command = "git ls-remote -h $gitURL"
+
+def proc = command.execute()
+proc.waitFor()
+
+if ( proc.exitValue() != 0 ) {
+   println "Error, ${proc.err.text}"
+   System.exit()
+}
+
+branches = proc.in.text.readLines().collect {
+  it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')}
+
+
+listhuist.addAll(branches)
+listhuist.unique()
+
+ listhuist.each {
+ println it
+}
