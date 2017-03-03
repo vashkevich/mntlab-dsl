8dfd0e3629816cbe0cdd878753178cce06a88727
hopetds <hopetds@gmail.com>
2017-03-03 11:13:16 +0300

    Delete work.sh

--- work.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf $WORKSPACE/*
-BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 7-)
-wget https://raw.githubusercontent.com/MNT-Lab/mntlab-dsl/master/script.sh
-chmod +x script.sh
-./script.sh>>output.txt
-tar -zcvf $BRANCH_NAME.tar.gz $WORKSPACE/script.sh
