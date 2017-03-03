2a7d8df477c469d613aae6d40f928459352f16a6
Student <student@epbyminw1969.minsk.epam.com>
2017-03-03 09:18:58 +0300

    friday morning

--- jobs.groovy
+++ jobs.groovy
@@ -10,6 +10,7 @@ job('MNTLAB-ikhamiakou-main-build-job') {
         }
         gitParam('BRANCH_NAME'){
             type('BRANCH')
+            defaultValue('origin/ikhamiakou')
         }
 
     }
@@ -24,7 +25,7 @@ job('MNTLAB-ikhamiakou-main-build-job') {
                     block {
                         buildStepFailure('FAILURE')
                         failure('FAILURE')
-                    unstable('UNSTABLE')
+                        unstable('UNSTABLE')
                     }
                     parameters {
                     predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
@@ -40,118 +41,36 @@ job('MNTLAB-ikhamiakou-main-build-job') {
 
 }
 
-job('MNTLAB-ikhamiakou-child1-build-job') {
+for (i in 1..4) {
+job('MNTLAB-ikhamiakou-child${i}-build-job') {
     
     parameters{
         activeChoiceReactiveParam('BRANCH_NAME') {
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('["ikhamiakou", "master"]')
+                script('["origin/ikhamiakou", "origin/master"]')
             }
         }
     }
     scm {
-        github('MNT-Lab/mntlab-dsl')
+        github('MNT-Lab/mntlab-dsl', '${BRANCH_NAME}')
     }
 
     steps {
         shell('''
+                rm -rf *.tar.gz
+                chmod +x script.sh
                 BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
                 bash script.sh >> output.txt
-                tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh output.txt '''
+                tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh '''
             )         
             
         publishers {
-        archiveArtifacts('*')
+        archiveArtifacts('*.tar.gz')
+        archiveArtifacts('output.txt')
+
         }
     }
     }   
 
-
-job('MNTLAB-ikhamiakou-child2-build-job') {
-    
-    parameters{
-        activeChoiceReactiveParam('BRANCH_NAME') {
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["ikhamiakou", "master"]')
-            }
-        }
-    }
-    scm {
-        github('MNT-Lab/mntlab-dsl')
-    }
-
-    steps {
-        shell('''
-                BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
-                bash script.sh >> output.txt
-                tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh output.txt '''
-            )         
-            
-        publishers {
-        archiveArtifacts('*')
-        }
-    }
-    }
-    
-    
-
-
-job('MNTLAB-ikhamiakou-child3-build-job') {
-    
-    parameters{
-        activeChoiceReactiveParam('BRANCH_NAME') {
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["ikhamiakou", "master"]')
-            }
-        }
-    }
-    scm {
-        github('MNT-Lab/mntlab-dsl')
-    }
-
-    steps {
-        shell('''
-                BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
-                bash script.sh >> output.txt
-                tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh output.txt '''
-            )         
-            
-        publishers {
-        archiveArtifacts('*')
-        }
-    }
-    }
-    
-    
-
-
-job('MNTLAB-ikhamiakou-child4-build-job') {
-    
-    parameters{
-        activeChoiceReactiveParam('BRANCH_NAME') {
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["ikhamiakou", "master"]')
-            }
-        }
-    }
-    scm {
-        github('MNT-Lab/mntlab-dsl')
-    }
-
-    steps {
-        shell('''
-                BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
-                bash script.sh >> output.txt
-                tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh output.txt '''
-            )         
-            
-        publishers {
-        archiveArtifacts('*')
-        }
-    }
-    }
-    
+}
