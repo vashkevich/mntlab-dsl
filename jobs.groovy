c625b4cdda549d55f66a4329a76f945b4dbd8737
Mikalai Kuzniatsou <mikalai_kuzniatsou@epam.com>
2017-03-03 13:04:37 +0300

    jobs.groovy adding comments

--- jobs.groovy
+++ jobs.groovy
@@ -1,26 +1,38 @@
-//Creating main build
+/* Jenkins. Task9. 
+DSL script for automated creation Jenkins jobs: 1 main and 4 child. */
+
+// Creating main job
 job("MNTLAB-mkuzniatsou-main-build") {
   	
-	
+// Getting project from Github	
     scm {
         github('MNT-Lab/mntlab-dsl', '$BRANCH_NAME')
-    }
-	
-    parameters {
-		gitParam('BRANCH_NAME') {
-            type('BRANCH')
-            defaultValue('origin/mkuzniatsou')
-        }
-		
-    activeChoiceParam('jobs') {
-            choiceType('CHECKBOX')
-            groovyScript {
-                script('return ["MNTLAB-mkuzniatsou-child1-build-job", "MNTLAB-mkuzniatsou-child2-build-job", "MNTLAB-mkuzniatsou-child3-build-job", "MNTLAB-mkuzniatsou-child4-build-job"]')
-            }
-    	}
-    }
+	}
 
-	
+	parameters {
+
+// Setting predefine string parameter BRANCH_NAME which has two options: {student} (by default) and master	
+        activeChoiceParam('BRANCH_NAME')
+			{
+                description('Allows to choose branch from repository')
+                choiceType('SINGLE_SELECT')
+                groovyScript
+                {
+                script('["mkuzniatsou", "master"]')
+                }
+            }
+			
+// Choosing jobs is executed by checkboxes 			
+		activeChoiceParam('jobs') 
+			{
+				choiceType('CHECKBOX')
+				groovyScript {
+				script('return ["MNTLAB-mkuzniatsou-child1-build-job", "MNTLAB-mkuzniatsou-child2-build-job", "MNTLAB-mkuzniatsou-child3-build-job", "MNTLAB-mkuzniatsou-child4-build-job"]')
+				}
+			}
+		}
+
+//Creating a trigger for launching 4 child jobs.	
 	steps {
         downstreamParameterized {
             trigger('$jobs') {
@@ -38,39 +50,63 @@ job("MNTLAB-mkuzniatsou-main-build") {
 }
 
 
-
+// Creating 4 child jobs
 for (i in 1..4) {
-  job("MNTLAB-mkuzniatsou-child${i}-build-job") {
+job("MNTLAB-mkuzniatsou-child${i}-build-job") {
 
 	scm {
 		github 'MNT-Lab/mntlab-dsl','${BRANCH_NAME}'
 	}
 	
-	parameters{
-        activeChoiceReactiveParam('BRANCH_NAME') {
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["mkuzniatsou", "master"]')
-            }
-        }
-    }
+// Setting parameter BRANCH_NAME with list of all branches available in repo.
+        parameters{
+			activeChoiceReactiveParam('BRANCH_NAME') {
+				choiceType('SINGLE_SELECT')
+				groovyScript {
 
+					script('''
+					def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+					def command = "git ls-remote -h $gitURL"
 
+					def proc = command.execute()
+					proc.waitFor()              
 
+					if ( proc.exitValue() != 0 ) {
+					   println "Error, ${proc.err.text}"
+					}
 
-	steps {
-	shell('''
-	rm -rf *.tar.gz
-	chmod +x script.sh
-        ./script.sh > output.txt
-	BRANCH_NAME=$(echo $BRANCH_NAME | cut -c 8-)
-        tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh'''
-	)
-
-	publishers {
-        archiveArtifacts('output.txt')
-        archiveArtifacts('*.tar.gz')
+					def branches = proc.in.text.readLines().collect { 
+						it.replaceAll(/[a-z0-9]*\trefs\\/heads\\//, '')
+					}
+					return branches'''
+					)	
+					
+					fallbackScript('''
+					BRANCH_NAME = "mkuzniatsou"'''
+					)					
+				}
 			}
 		}
-	}
+
+		
+        steps {
+ 
+/* Removing old artifacts
+Executing the cloned ‘script.sh’ from branch propagated by main job. Saving an output to the file ‘output.txt’
+Creating artifact
+*/
+        shell('''
+		rm -rf *.tar.gz
+		chmod +x script.sh
+        ./script.sh > output.txt
+		tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy script.sh'''
+		)
+
+// Archiving artifacts
+		publishers {
+				archiveArtifacts('output.txt')
+				archiveArtifacts('*.tar.gz')
+        }       
+        }
+    }
 }
